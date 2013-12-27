e05c600b0cf79ec38ae470b1182cbd71a9712de6
zzpwelkin <zhangzupeng19871203@126.com>
2013-12-27 15:36:12 +0800

    fix

--- conf.d/celeryconfig.py
+++ conf.d/celeryconfig.py
@@ -5,4 +5,4 @@ CELERY_RESULT_BACKEND = "mongodb"
 CELERY_RESULT_DBURI = os.environ.get('OPENSHIFT_MONGODB_DB_URL')
 
 CELERY_IMPORTS = ('celerytks', )
-CELERYD_LOG_FILE = '{0}log/celery/worker1.log'.format(os.environ.get('OPENSHIFT_TMP_DIR'))
+#CELERYD_LOG_FILE = '{0}log/celery/worker1.log'.format(os.environ.get('OPENSHIFT_TMP_DIR'))
