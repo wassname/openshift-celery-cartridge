a713eda82bc8863219d2491d3f00a0b17dea423e
zzpwelkin <zhangzupeng19871203@126.com>
2013-12-27 15:21:48 +0800

    configure file

---
+++ conf.d/celeryconfig.py
@@ -0,0 +1,8 @@
+import os
+
+BROKER_URL = os.environ.get('OPENSHIFT_MONGODB_DB_URL')
+CELERY_RESULT_BACKEND = "mongodb"
+CELERY_RESULT_DBURI = os.environ.get('OPENSHIFT_MONGODB_DB_URL')
+
+CELERY_IMPORTS = ('celerytks', )
+CELERYD_LOG_FILE = '{0}log/celery/worker1.log'.format(os.environ.get('OPENSHIFT_TMP_DIR'))
