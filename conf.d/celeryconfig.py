f1b72f81396edff9485b79d4bef76550e8fdb013
zzpwelkin <zhangzupeng19871203@126.com>
2013-12-27 18:03:10 +0800

    Update celeryconfig.py

--- conf.d/celeryconfig.py
+++ conf.d/celeryconfig.py
@@ -6,6 +6,6 @@ CELERY_RESULT_DBURI = os.environ.get('OPENSHIFT_MONGODB_DB_URL')
 
 celery_imports = os.environ.get('OPENSHIFT_CELERY_IMPORTS')
 
-CELERY_IMPORTS = celery_import.split(',') if celery_imports else ('celerytks',)
+CELERY_IMPORTS = celery_imports.split(',') if celery_imports else ('celerytks',)
 
 CELERYD_LOG_FILE = '{0}log/celery/worker1.log'.format(os.environ.get('OPENSHIFT_TMP_DIR'))
